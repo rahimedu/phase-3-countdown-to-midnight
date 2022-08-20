6e2d95095bb991968e0060ecc7231e07e2c1defb
rahimedu <edwin.duche82@gmail.com>
2022-08-20 16:20:31 -0400

    commit tree 779e65ea23d6e639eb016a3cc1ec7b780782981c

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+def countdown(midnight)
+    while midnight > 0
+        puts "#{midnight} SECOND(S)!"
+        midnight -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(integer)
+    while integer > 0
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
